46162e4fbecba8f817948c90aa0969ac44044299
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-03-23 14:00:21 -0500

    1.2.0.0

--- Source/Log.cs
+++ Source/Log.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace ODFC
+namespace FieldTrainingFacility
 {
     class Log
     {
@@ -21,114 +21,7 @@ namespace ODFC
             if (0 != @params.Length) msg = string.Format(msg, @params);
             ScreenMessages.PostScreenMessage(msg, 1, ScreenMessageStyle.UPPER_CENTER, true);
             //Logg.Info(msg);
-            //? UnityEngine.Debug.Log("[ODFC TweakScale] " + msg);
-        }
-    }
-    internal static class Logg
-    {
-        public enum LEVEL
-        {
-            OFF = 0,
-            ERROR = 1,
-            WARNING = 2,
-            INFO = 3,
-            DETAIL = 4,
-            TRACE = 5
-        };
-
-        public static LEVEL level = LEVEL.INFO;
-
-        private static readonly String PREFIX = "ODFC" + ": ";
-
-        public static LEVEL GetLevel()
-        {
-            return level;
-        }
-
-        public static void SetLevel(LEVEL level)
-        {
-            UnityEngine.Debug.Log("log level " + level);
-            Logg.level = level;
-        }
-
-        public static LEVEL GetLogLevel()
-        {
-            return level;
-        }
-
-        private static bool IsLevel(LEVEL level)
-        {
-            return level == Logg.level;
-        }
-
-        public static bool IsLogable(LEVEL level)
-        {
-            return level <= Logg.level;
-        }
-
-        public static void Trace(String msg)
-        {
-            if (IsLogable(LEVEL.TRACE))
-            {
-                UnityEngine.Debug.Log(PREFIX + msg);
-            }
-        }
-
-        public static void Detail(String msg)
-        {
-            if (IsLogable(LEVEL.DETAIL))
-            {
-                UnityEngine.Debug.Log(PREFIX + msg);
-            }
-        }
-
-        [ConditionalAttribute("DEBUG")]
-        public static void Info(String msg)
-        {
-            if (IsLogable(LEVEL.INFO))
-            {
-                UnityEngine.Debug.Log(PREFIX + msg);
-            }
-        }
-
-        [ConditionalAttribute("DEBUG")]
-        public static void Test(String msg)
-        {
-            //if (IsLogable(LEVEL.INFO))
-            {
-                UnityEngine.Debug.LogWarning(PREFIX + "TEST:" + msg);
-            }
-        }
-
-        public static void Warning(String msg)
-        {
-            if (IsLogable(LEVEL.WARNING))
-            {
-                UnityEngine.Debug.LogWarning(PREFIX + msg);
-            }
-        }
-
-        public static void Error(String msg)
-        {
-            if (IsLogable(LEVEL.ERROR))
-            {
-                UnityEngine.Debug.LogError(PREFIX + msg);
-            }
-        }
-
-        public static void Debug(bool debugMode, String msg)
-        {
-            if (debugMode)
-            {
-                UnityEngine.Debug.LogError(PREFIX + msg);
-            }
-            else
-                Logg.Info(msg);
-        }
-
-        public static void Exception(Exception e)
-        {
-            Logg.Error("exception caught: " + e.GetType() + ": " + e.Message);
+            //? UnityEngine.Debug.Log("[FTL: FieldTrainingFacility] " + msg);
         }
     }
 }
--- Source/Properties/Version.cs
+++ Source/Properties/Version.cs
@@ -6,10 +6,10 @@ namespace FieldTrainingFacility
     public static class Version
     {
         public const int major = 1;
-        public const int minor = 0;
-        public const int patch = 3;
-        public const int build = 5;
-        public const string Number = "1.0.3.5";
+        public const int minor = 2;
+        public const int patch = 0;
+        public const int build = 0;
+        public const string Number = "1.2.0.0";
 #if DEBUG
         public const string Text = Number + " BETA DEBUG";
 #else
