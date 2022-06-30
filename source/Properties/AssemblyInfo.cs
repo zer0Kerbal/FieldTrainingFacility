18b323c63f52347109873e0000941455822b6a49
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-06-29 23:12:10 -0500

    Update AssemblyInfo.cs

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -1,18 +1,34 @@
-﻿using System.Reflection;
+﻿/* Field Training Facility (FTF)
+ * Kerbals gain experience (stars) using time and electric charge. For Kerbal Space Program.
+ * Copyright (C) 2016 EFour
+ * Copyright (C) 2019, 2022 zer0Kerbal (zer0Kerbal at hotmail dot com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Field Training Facility (FTL)")]
-[assembly: AssemblyDescription("Train Kerbals using time and Electric Charge for Kerbal Space Program")]
+[assembly: AssemblyTitle("Field Training Facility (FTF)")]
+[assembly: AssemblyDescription("Train Kerbals using time and Electric Charge, for Kerbal Space Program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kerbalnaut Training Industries, Inc.")]
 [assembly: AssemblyProduct("Field Training Facility (FTL)")]
-[assembly: AssemblyCopyright("Copyright © 2016 Efour")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Efour (c) 2019, 2022 zer0Kerbal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("f1e7665d-05fe-4267-85e8-fda6d510628e")]
-[assembly: AssemblyInformationalVersionAttribute("Train Kerbals using time and Electric Charge for Kerbal Space Program")]
-// [assembly: AssemblyVersion("1.0.*")]
-// [assembly: AssemblyFileVersion("1.0.3.4")]
